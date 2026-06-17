5efda563ae520af27907fac2684fd771756826da
Bhargav <bhargavkallepally9@gmail.com>
2026-06-17 23:03:51 +0530

    Feat: Added docker files

--- cipher-guess-backend/src/main/java/com/bhargav/crack_the_number/CrackTheNumberApplication.java
+++ cipher-guess-backend/src/main/java/com/bhargav/crack_the_number/CrackTheNumberApplication.java
@@ -1,7 +1,10 @@
 package com.bhargav.crack_the_number;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class CrackTheNumberApplication {
@@ -9,5 +12,12 @@ public class CrackTheNumberApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrackTheNumberApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner test(Environment env) {
+		return args -> {
+			System.out.println("DB_URL: " + env.getProperty("DB_URL"));
+			System.out.println("Datasource URL: " +
+					env.getProperty("spring.datasource.url"));
+		};
+	}
 }
